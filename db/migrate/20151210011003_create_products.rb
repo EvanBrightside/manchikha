29f336134cf840748ec24d40d4d54d0837fa2b3f
EvanBrightside <xvanx84@gmail.com>
2015-12-10 21:05:17 +0400

    New Product and Category models, also Validators and relationships were added

--- db/migrate/20151210011003_create_products.rb
+++ db/migrate/20151210011003_create_products.rb
@@ -2,6 +2,10 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.integer :category_id
+      t.string :name
+      t.text :description
+
+      t.refference :catefory, null: false
 
       t.timestamps
     end
