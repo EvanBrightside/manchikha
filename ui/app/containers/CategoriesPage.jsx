61941b6d6bf80de293f7169b2d09fba4fcb3f87a
EvanBrightside <xvanx84@gmail.com>
2016-07-11 11:28:57 +0300

    Fix indention tab_size 2

--- ui/app/containers/CategoriesPage.jsx
+++ ui/app/containers/CategoriesPage.jsx
@@ -6,27 +6,27 @@ export default class CategoriesPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-		categories: [],
-    		currentCategory: { products: [] }
+			categories: [],
+			currentCategory: { products: [] }
 		};
-  }
+	}
 
-  componentDidMount() {
-    request
-        .get('/api/categories')
-        .end((err, res) => {
-            const response = JSON.parse(res.text);
-            this.setState({
-                categories: response.categories,
-                currentCategory: response.categories[0]
-            });
-        });
+ 	componentDidMount() {
+		request
+			.get('/api/categories')
+			.end((err, res) => {
+				const response = JSON.parse(res.text);
+				this.setState({
+					categories: response.categories,
+					currentCategory: response.categories[0]
+				});
+			});
 	}
 
 	render() {
-    return (
-        <CategoriesList categories={this.state.categories} />
-        <ProductsList category={this.state.currentCategory} />
-    )
-  }
+		return (
+			<CategoriesList categories={this.state.categories} />
+			<ProductsList category={this.state.currentCategory} />
+		)
+	}
 }
